aa195ca3741e4d802841008bea2c6f0e7932fe84
unknown <Milad>
2024-04-06 10:31:08 +0330

    please Work!

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-require('express-async-errors');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
-const groupRoutes = require('./routes/group');
-const postRoutes = require('./routes/post');
-const userRoutes = require('./routes/user');
+const express = require("express");
+require("express-async-errors");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const groupRoutes = require("./routes/group");
+const postRoutes = require("./routes/post");
+const userRoutes = require("./routes/user");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({limit: '10mb', extended: true}));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use('/api', authRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/groups', groupRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/groups", groupRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
